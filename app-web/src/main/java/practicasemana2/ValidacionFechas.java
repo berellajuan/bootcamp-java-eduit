45ed89a1c175ccc31e7236b2d79cdf98067df9b8
berellajuan <berellajuancruz@hotmail.com>
2022-04-20 20:29:27 -0300

    -Correccion final

--- app-web/src/main/java/practicasemana2/ValidacionFechas.java
+++ app-web/src/main/java/practicasemana2/ValidacionFechas.java
@@ -19,96 +19,44 @@ public class ValidacionFechas {
 		int mes = teclado.nextInt();
 		System.out.print("Ingrese Año: ");
 		int anio = teclado.nextInt();
-		if(validarFecha(dia, mes, anio)) {
-			System.out.println("La fecha: " + dia +"/"+ mes + "/" + anio +" Es CORRECTA");
-		}else {
-			System.out.println("La fecha: " + dia +"/"+ mes + "/" + anio +" Es INCORRECTA");
+		if (validarFecha(dia, mes, anio)) {
+			System.out.println("La fecha: " + dia + "/" + mes + "/" + anio + " Es CORRECTA");
+		} else {
+			System.out.println("La fecha: " + dia + "/" + mes + "/" + anio + " Es INCORRECTA");
 		}
-		
+
 		teclado.close();
-		
-		
+
 	}
-	
-	public static boolean validarFecha(int dia,int mes, int anio) {
-		boolean res=false;
-		if(dia > 0 && anio >= 1900 && anio <= 2099 && mes >= 1 && mes <= 12) {
-			
-			switch (mes) {
-			case 1:
-				if(dia <=31) {
+
+	public static boolean validarFecha(int dia, int mes, int anio) {
+		boolean res = false;
+		if (dia > 0 && anio >= 1900 && anio <= 2099 && mes >= 1 && mes <= 12) {
+			if (mes == 2) {
+				if (!esBisiesto(anio) && dia >= 29) {
+					res = false;
+				} else if (dia <= 29) {
 					res = true;
 				}
-				break;
-			case 2:
-				if(!esBisiesto(anio) && dia >= 29) {
-					res=false;
-				}else if(dia <= 29){
-					res=true;
-				}
-				break;
-			case 3:
-				if(dia <=31) {
-					res=true;
-				}
-				break;
-			case 4:
-				if(dia <= 30) {
+			}
+			if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
+				if (dia <= 31) {
 					res = true;
 				}
-				break;
-			case 5:
-				if(dia <= 31) {
+			}
+			if (mes == 4 || mes == 6  || mes == 9 || mes == 11) {
+				if (dia <= 30) {
 					res = true;
 				}
-				break;
-			case 6:
-				if(dia <= 30) {
-					res = true;
-				}
-				break;
-			case 7:
-				if(dia <= 31) {
-					res = true;
-				}
-				break;
-			case 8:
-				if(dia <= 31) {
-					res = true;
-				}
-				break;
-			case 9:
-				if(dia <= 30) {
-					res = true;
-				}
-				break;
-			case 10:
-				if(dia <= 31) {
-					res = true;
-				}
-				break;
-			case 11:
-				if(dia <= 30) {
-					res = true;
-				}
-				break;
-			case 12:
-				if(dia <= 31) {
-					res = true;
-				}
-				break;
-			
 			}
 		}
-		
+
 		return res;
 	}
-	
-	//Valida si es Biciesto
+
+	// Valida si es Biciesto
 	public static boolean esBisiesto(int anio) {
-		return anio % 4 == 0 && anio % 100 != 0|| anio % 400 == 0;
+		return anio % 4 == 0 && anio % 100 != 0 || anio % 400 == 0;
 	}
-	
+
 }
-
-
